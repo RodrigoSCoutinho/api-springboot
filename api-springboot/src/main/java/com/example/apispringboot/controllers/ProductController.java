8b486db514ef9edab6646beb1b7d0cca37fb7ecc
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-09-27 10:52:09 -0300

    "feature/created productRecordDTO and method Post"

--- api-springboot/src/main/java/com/example/apispringboot/controllers/ProductController.java
+++ api-springboot/src/main/java/com/example/apispringboot/controllers/ProductController.java
@@ -1,8 +1,27 @@
 package com.example.apispringboot.controllers;
 
-
+import com.example.apispringboot.dtos.ProductRecordDto;
+import com.example.apispringboot.models.ProductModel;
+import com.example.apispringboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
