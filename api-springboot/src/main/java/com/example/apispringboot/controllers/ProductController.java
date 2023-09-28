6ccbced1f95d204d4fbbb5d46673c3427d2b2da4
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-09-28 11:56:03 -0300

    feature/method-delete

--- api-springboot/src/main/java/com/example/apispringboot/controllers/ProductController.java
+++ api-springboot/src/main/java/com/example/apispringboot/controllers/ProductController.java
@@ -54,4 +54,13 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
 
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productRepository.delete(product0.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
 }
